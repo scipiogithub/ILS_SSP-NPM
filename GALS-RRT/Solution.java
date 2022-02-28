c8bb05e99f37aacf28d0cd1653d9b7bebb604ab7
scipiogithub <54682763+scipiogithub@users.noreply.github.com>
2022-02-28 18:55:20 +0300

    Update Solution.java

--- GALS-RRT/Solution.java
+++ GALS-RRT/Solution.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gals;
 
 import java.util.ArrayList;
@@ -172,7 +168,6 @@ public class Solution {
                     break;
                 }
             }
-            //j1 = rnd.nextInt(machines[m1].jobs.size());
         }
 
         Solution newSolution = new Solution(NONE);
@@ -222,7 +217,7 @@ public class Solution {
         return newSolution;
     }
     
-    //group insert
+    //block insert
 
     public Solution insert(int into, int beforejob, int from, int job, int tojob) {
         if (OBJECTIVE == FMAX && machines[from].flowtime < fitness) {
