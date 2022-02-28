88ea8d275b7f9c1c938253cde4a5f7a814ece300
scipiogithub <54682763+scipiogithub@users.noreply.github.com>
2022-02-28 19:00:18 +0300

    Update Worker.java

--- TSB/Worker.java
+++ TSB/Worker.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gals;
 
 import java.util.ArrayList;
@@ -58,7 +54,6 @@ public class Worker {
             Solution ls = op <= operator[0] ? solution.swap(m1, j1, m2, j2) //: solution.insert(m1, j1, m2, j2)
                     : (op <= operator[1] ? solution.insert(m1, j1, m2, j2)
                             : solution.insert(m1, j1, m2, j2, j3));
-            //: solution.reset(m1, j1)));
             if (ls.fitness < solution.fitness) {
                 counter = 0;
                 solution = ls;
